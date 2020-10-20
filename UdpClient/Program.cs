590628c2175020456bcbbfb9f42990b089b310e9
Medina L <lars.medina71@gmail.com>
2020-10-20 12:29:36 +0200

    Add project files.

---
+++ UdpClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpEchoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "";
+            byte[] sendBytes = new Byte[1024];
+            byte[] rcvPacket = new Byte[1024];
+            UdpClient client = new UdpClient();
+            
+            client.Connect(IPAddress.Parse("127.0.0.1"), 1000);
+            IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("Client is Started");
+            Console.WriteLine("Type your message");
+
+            while (data != "q")
+            {
+                data = Console.ReadLine();
+                sendBytes = Encoding.ASCII.GetBytes(DateTime.Now + " " + data);
+                client.Send(sendBytes, sendBytes.GetLength(0));
+                rcvPacket = client.Receive(ref remoteIPEndPoint);
+
+                string rcvData = Encoding.Default.GetString(rcvPacket);
+                Console.WriteLine("Handling client at " + remoteIPEndPoint + " - ");
+
+                Console.WriteLine("Message Received: " + rcvData);
+            }
+            Console.WriteLine("Close Port Command Sent");  //user feedback
+            Console.ReadLine();
+            client.Close();  //close connection
+
+        }
+    }
+}
+    
---
+++ UdpTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpEchoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "";
+
+            UdpClient server = new UdpClient(1000);
+
+
+            IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+
+            Console.WriteLine(" S E R V E R   IS   S T A R T E D ");
+            Console.WriteLine("* Waiting for Client...");
+            while (data != "q")
+            {
+                byte[] receivedBytes = server.Receive(ref remoteIPEndPoint);
+                data = Encoding.ASCII.GetString(receivedBytes);
+                Console.WriteLine("Handling client at " + remoteIPEndPoint + " - ");
+                Console.WriteLine("Message Received " + data.TrimEnd());
+
+                server.Send(receivedBytes, receivedBytes.Length, remoteIPEndPoint);
+                Console.WriteLine("Message Echoed to" + remoteIPEndPoint + data);
+            }
+
+            Console.WriteLine("Press Enter Program Finished");
+            Console.ReadLine(); //delay end of program
+            server.Close();  //close the connection
+        }
+    }
+}
