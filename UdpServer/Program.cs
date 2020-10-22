25fd60ad15c7595aeceee6ab6aeaf34e7e0a4efa
Medina L <lars.medina71@gmail.com>
2020-10-22 21:08:59 +0200

    Can reproduce the problem

--- UdpServer/Program.cs
+++ UdpServer/Program.cs
@@ -2,6 +2,8 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace UdpEchoServer
 {
@@ -10,29 +12,48 @@ namespace UdpEchoServer
        public static void Main(string[] args)
         {
             string data = "";
-
-            UdpClient server = new UdpClient(1000);
-
-
-            IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
-
+            bool isRunning = true;
 
             Console.WriteLine(" S E R V E R   IS   S T A R T E D ");
             Console.WriteLine("* Waiting for Client...");
-            while (data != "q")
-            {
-                byte[] receivedBytes = server.Receive(ref remoteIPEndPoint);
-                data = Encoding.ASCII.GetString(receivedBytes);
-                Console.WriteLine("Handling client at " + remoteIPEndPoint + " - ");
-                Console.WriteLine("Message Received " + data.TrimEnd());
 
-                server.Send(receivedBytes, receivedBytes.Length, remoteIPEndPoint);
-                Console.WriteLine("Message Echoed to" + remoteIPEndPoint + data);
-            }
+            UdpClient server1 = new UdpClient(1000);
+            UdpClient server2 = new UdpClient();
+
+            IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            var m = new ManualResetEventSlim();
+            m.Reset();
+
+            Task.Run(() =>
+            {
+                while (isRunning)
+                {
+                    Console.WriteLine("Rx waiting");
+                    byte[] receivedBytes = server1.Receive(ref remoteIPEndPoint);
+                    data = Encoding.ASCII.GetString(receivedBytes);
+                    Console.WriteLine("Handling client at " + remoteIPEndPoint + " - ");
+                    Console.WriteLine("Message Received " + data.TrimEnd());
+                    m.Set();
+                }
+            });
+
+            Task.Run(() =>
+            {
+                while (isRunning)
+                {
+                    m.Wait();
+                    m.Reset();
+                    var b = Encoding.Default.GetBytes(data);
+                    server2.Send(b, b.Length, remoteIPEndPoint);
+                    Console.WriteLine("Message Echoed to" + remoteIPEndPoint + data);
+                }
+            });
 
             Console.WriteLine("Press Enter Program Finished");
             Console.ReadLine(); //delay end of program
-            server.Close();  //close the connection
+            server2.Close();  //close the connection
+            server1.Close();
         }
     }
 }
